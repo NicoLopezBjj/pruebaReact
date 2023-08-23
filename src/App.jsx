af4893900ab9838eb7969102b2da35785c9b65c3
Nico <lopeznnr@gmail.com>
2023-08-23 11:22:38 -0300

    separe la logica de los useeffect y agregue boton para el fact

--- src/App.jsx
+++ src/App.jsx
@@ -10,13 +10,20 @@ function App() {
   const [fact, setFact] = useState()
   const [imageUrl, setImageUrl] = useState()
 
+// PARA RECUPERAR O ACTUALIZAR EL FACT DEL GATO
   useEffect (()=>{
     fetch(PRIMER_API_RANDOM_FACT)
     .then(res => res.json())
     .then(data => {
       const {fact} = data
       setFact(fact)
-      
+      })
+  }, [])
+  
+// PARA ACTUALIZAR LA IMAGEN DEL GATO
+  useEffect (()=>{
+      if (!fact) return
+
       const threeWords = fact.split(' ',3)
       console.log(threeWords)
 
@@ -27,15 +34,25 @@ function App() {
       .then(blob => {
         const imageUrl = URL.createObjectURL(blob)
         setImageUrl(imageUrl)
-      }) 
-    
-    })
-  }, []) 
+      })
+  }, [fact])
+       
+  
+
+const handleClick = () => {
+    fetch(PRIMER_API_RANDOM_FACT)
+    .then(res => res.json())
+    .then(data => {
+      const {fact} = data
+      setFact(fact)
+      })
+  }
 
   return (
-    <main>
-      <h1>App de gatitos</h1>
-      {fact && <p>{fact}</p>}
+    <main className='container card'>
+      <h3 className='card-title'>App de gatitos</h3>
+      {fact && <p className='card-body'>{fact}</p>}
+      <button onClick={handleClick} className='btn btn-secondary mb-2'>Get a new fact</button>
       {imageUrl && <img src={`${imageUrl}`} alt={`Image extracted using the threewords for ${fact}`} />}
     </main>
   )
